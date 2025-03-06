5ba4c2af5fc9701dea7eec6a3dff3a7941fcc2fb
qcsstudio <qcraftstudio@gmail.com>
2025-03-06 17:19:19 +0530

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import DashboardContainer from "@/containers/DashboardContainer/DashboardContainer";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="homeContainer">
-      Home
+            <DashboardContainer/>
     </div>
   );
 }
