798d5a592a27061a6da3de52772a5b743d4b5ba6
Omkar Khallar <omkarkhallar64@gmail.com>
2025-03-12 10:53:31 +0530

    unknow fix

--- src/components/DashboardComponents/DashboardComponent.jsx
+++ src/components/DashboardComponents/DashboardComponent.jsx
@@ -11,20 +11,20 @@ import HomeComponent from './HomeComponent';
 import { dashboardContext } from '@/Context/Dashboard.context';
 
 const DashboardComponent = () => {
-  
 
-  const {currentComponent,setCurrentComponent} = useContext(dashboardContext);
+
+  const { currentComponent, setCurrentComponent } = useContext(dashboardContext);
 
   const renderComponent = () => {
     switch (currentComponent) {
       case 'home':
-        return <HomeComponent/>;
+        return <HomeComponent />;
       case 'accounts':
         return <AccountComponent />;
       case 'createpost':
         return <CreatePost />;
       case 'schedulepost':
-        return <ScheduledPost/>;
+        return <ScheduledPost />;
       case 'analytics':
         return <AnalyticsComponent />;
       case 'engagement':
@@ -32,29 +32,20 @@ const DashboardComponent = () => {
       case 'settings':
         return <Settings />;
       default:
-        return <HomeComponent/>;
+        return <HomeComponent />;
     }
   };
-
-  //insights chart 
-
-  
 
   // Componet 
   return (
     <div className='relative h-[100%] px-[1.5rem] py-5  '>
-
-      <div className=' '>
-
-        <div className=''>
-          <div className='flex items-center gap-1 py-6'>
-            <h1 className='font-bold text-[30px]'>Welcome QCS,</h1>
-            <p className='text-[#9E9E9E] text-[22px] tracking-wide'>Let’s optimize your social media today</p>
-          </div>
-          {renderComponent()}
-        </div>
+      <div className='flex items-center gap-1 py-6'>
+        <h1 className='font-bold text-[30px]'>Welcome QCS,</h1>
+        <p className='text-[#9E9E9E] text-[22px] tracking-wide'>Let’s optimize your social media today</p>
       </div>
+      {renderComponent()}
     </div>
+
   )
 }
 
