460a2dc1ca992effe6ee2377a79a9fc8d6c26055
Omkar Khallar <omkarkhallar64@gmail.com>
2025-03-20 16:39:33 +0530

    omkar/ cloud added

---
+++ src/data/homePageSlider.data.js
@@ -0,0 +1,38 @@
+export const sliderData = [
+
+    {
+        id:1,
+        heading: "Financial Security",
+        description:"A financial institution faced frequent security breaches, compromising customer data. TAC Security implemented a comprehensive risk-based vulnerability management system and real-time threat intelligence, leading to a 60% reduction in security incidents and successful regulatory compliance audits within six months.",
+        url:"#",
+        btnText:"Get Started",
+    },
+    {
+        id:2,
+        heading: "Healthcare Security",
+        description:"A healthcare provider struggled with increasing cyberattacks targeting sensitive patient data. TAC Security deployed secure infrastructure solutions and continuous monitoring for early threat detection, reducing cyberattacks by 80% while ensuring HIPAA compliance and safeguarding patient confidentiality.",
+        url:"#",
+        btnText:"Get Started",
+    },
+    {
+        id:3,
+        heading: "E-Commerce Security",
+        description:"An e-commerce platform was targeted by multiple DDoS attacks, affecting user experience. TAC Security conducted penetration testing and fortified the platform’s infrastructure against future threats, eliminating downtime and enhancing security, leading to a 40% increase in customer trust and sales.",
+        url:"#",
+        btnText:"Get Started",
+    },
+    {
+        id:4,
+        heading: "Government Compliance",
+        description:"A government agency struggled with meeting complex security regulations and compliance requirements. TAC Security helped the agency implement solutions to align with regulatory standards, ensuring full compliance and preventing data leaks while enhancing overall security posture.",
+        url:"#",
+        btnText:"Get Started",
+    },
+    {
+        id:5,
+        heading: "Global Retailer’s",
+        description:"A global retailer faced increasing cyber threats targeting customer payment data and internal systems. TAC Security implemented end-to-end vulnerability management, enhanced threat intelligence, and secure payment systems, resulting in zero data breaches over 12 months and boosting customer confidence by 25%.",
+        url:"#",
+        btnText:"Get Started",
+    },
+]
