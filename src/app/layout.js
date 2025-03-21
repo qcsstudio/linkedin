aeff2b9f3700f6f73622b2893e86c63ad7b0e7da
qcsstudio <qcraftstudio@gmail.com>
2025-03-21 17:49:19 +0530

    seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import DashboardContextProvider from "@/Context/Dashboard.context";
 import { PrimeReactProvider } from "primereact/api";
 
 import { UserContextProvider } from "@/Context/user.context";
-
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,22 +19,45 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "ElevatrX | AI-Powered Social Media Automation Platform",
-  description: "Automate your social media content creation, scheduling, and analytics with ElevatrX. Enhance your brand visibility effortlessly—Try it free today!",
+  description:
+    "Automate your social media content creation, scheduling, and analytics with ElevatrX. Enhance your brand visibility effortlessly—Try it free today!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="FE1jcS7Gi8yAWxnnLZ-lweGrna3FI5zh61v14-5q1Og" />
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KLNTT48L');
+            `,
+        }}
+      />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLNTT48L"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
 
-<PrimeReactProvider >
-        <UserContextProvider>
-          <DashboardContextProvider>{children}</DashboardContextProvider>
-        </UserContextProvider>
+        <PrimeReactProvider>
+          <UserContextProvider>
+            <DashboardContextProvider>{children}</DashboardContextProvider>
+          </UserContextProvider>
         </PrimeReactProvider>
-
       </body>
     </html>
   );
