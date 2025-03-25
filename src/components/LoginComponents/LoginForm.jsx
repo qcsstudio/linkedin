bb0da76574e496573216e8bf8c780481ed77ddc8
qcsstudio <qcraftstudio@gmail.com>
2025-03-25 18:24:39 +0530

    dep error

--- src/components/LoginComponents/LoginForm.jsx
+++ src/components/LoginComponents/LoginForm.jsx
@@ -15,6 +15,7 @@ import { Navigation, Pagination, Scrollbar, A11y,Autoplay } from "swiper/modules
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
