9333279ed54988259d703e84d3b326e915561139
Omkar Khallar <omkarkhallar64@gmail.com>
2025-04-10 17:23:55 +0530

    navjot/ font change

--- src/containers/HomeContainer/HomeContainer.jsx
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -19,7 +19,7 @@ import CloudSection from "@/components/HomeComponent/CloudSection/CloudSection";
 const HomeContainer = () => {
   return (
     <>
-      <div className="font-nats mainContainer relative bg-[url(/images/homeImages/grain.png)] bg-cover  bg-no-repeat  w-[100%] min-h-[100vh] overflow-hidden">
+      <div className="mainContainer font-syne relative bg-[url(/images/homeImages/grain.png)] bg-cover  bg-no-repeat  w-[100%] min-h-[100vh] overflow-hidden">
         <div className="bg-[#5E788F]/85 flex flex-col gap-3 md:gap-6 lg:gap-10">
           <Navbar />
           <HeroSection />
